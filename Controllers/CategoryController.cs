3c7b069454d5266d23e4c2bc72b03f12e05cfac0
ronay topal <ronaytopal@ubutu2.onmicrosoft.com>
2023-08-05 00:58:45 +0300

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            // Create a list of Category type
+            // Retrieve categories in a list from database and assign it to objCategoryList 
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            //_db.Categories.Add(obj);
+            //_db.SaveChanges();
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Category obj)
+        {
+            // Ensure all requirements are met defined in associated Model
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
+
+
+        }
+    }
+}
+ 
---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key] // Key attribute assigns 'ID' as the primary key
+        public int Id { get; set; }
+
+        [Required] // 
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        // Accept a number between numbers specified below
+        [Range(1,100)]
+        public int DisplayOrder { get; set; }
+
+    }
+}
